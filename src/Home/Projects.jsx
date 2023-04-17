766e7d44b4459f62c0e814f30a4a01fd20facb56
GerVaf <thantzinmin667@gmail.com>
2023-04-17 16:54:16 +0630

    v1.4

--- src/Home/Projects.jsx
+++ src/Home/Projects.jsx
@@ -8,7 +8,7 @@ const Projects = () => {
       <div className="p-5 flex flex-col gap-5 ">
         {ProjectsDisplay.map((pj) => {
           return (
-            <div className="card lg:card-side rounded-sm bg-base-100 shadow-xl bg-slate-100">
+            <div className="card lg:card-side rounded-sm shadow-xl bg-slate-100 h-[350px]">
               <figure>
                 <img className=" rounded-t-sm" src={pj.img} alt="Album" />
               </figure>
@@ -19,12 +19,14 @@ const Projects = () => {
                     {pj.title}
                   </h2>
                 </div>
-                <div className="flex gap-5 my-4">
+
+                {/* include */}
+                <div className=" flex gap-5 my-4">
                   {pj.language.map((lang) => {
                     return <img className=" w-8" src={lang.img} alt="" />;
                   })}
                 </div>
-                <div className="flex text-2xl">
+                <div className="flex text-2xl text-black">
                   <div className="flex justify-center w-1/2 hover:border-b hover:bg-slate-200 hover:border-[#FF4848] hover:transition p-2 ">
                   <a >
                   <AiFillGithub/>
